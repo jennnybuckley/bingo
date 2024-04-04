560ba9776c0aeda9029d6d315e828975a0b99b52
jennnybuckley <46970676+jennnybuckley@users.noreply.github.com>
2024-04-04 18:03:21 -0400

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,31 @@
+function love.load()
+    x = 400
+    y = 300
+    speed = 200
+end
+
+function love.update(dt)
+    dx = 0
+    dy = 0
+    if love.keyboard.isDown("right") then
+        dx = dx + speed
+    end
+    if love.keyboard.isDown("left") then
+        dx = dx - speed
+    end
+    if love.keyboard.isDown("down") then
+        dy = dy + speed
+    end
+    if love.keyboard.isDown("up") then
+        dy = dy - speed
+    end
+    d = math.sqrt(dx * dx + dy * dy)/speed
+    if (d > 0) then
+        x = x + dt*dx/d
+        y = y + dt*dy/d
+    end
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    love.graphics.print("Hello World", x, y)
 end
